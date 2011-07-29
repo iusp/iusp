9617ef7d7e3373f412c119af2a1c66b02b6ea038
IUSP <iusp@iusp.ru>
2011-07-29 08:38:20 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iusp::Application.config.secret_token = 'bb05f3f976de06b57385fd5dcfe0cac16ff25c7eca646d09651ee77a76f82567c1628fd080c14f90f0aaedabcb04366b04b8346dbf3338f3b54aba55a31fc2ad'
